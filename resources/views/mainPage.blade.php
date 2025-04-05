6a12abfc28ac792a23096997a0502007495af66b
Jeweliris <166207696+Jeweliris@users.noreply.github.com>
2025-04-05 22:00:10 +0800

    Update mainPage.blade.php

--- resources/views/mainPage.blade.php
+++ resources/views/mainPage.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>JEWEL</title>
+    <title>Document</title>
     @vite('resources/css/mainPage.css')
     @vite('resources/js/mainPage.js')
     <style>
