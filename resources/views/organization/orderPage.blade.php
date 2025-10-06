77677b2a3afba957170db8b17340a69949ecdc70
JhedAndreiM <jhedandreimagdato06@gmail.com>
2025-10-06 18:34:42 +0800

    Refactor Modal Styles and Enhance Screenshot Functionality
    - Updated modal styles for improved positioning, layout, and visual consistency.
    - Removed unnecessary SVG elements to streamline the modal structure.
    - Enhanced screenshot functionality with error handling for the html2canvas library.
    - Adjusted CSS for better responsiveness and user experience in the order page.

--- resources/views/organization/orderPage.blade.php
+++ resources/views/organization/orderPage.blade.php
@@ -4,6 +4,9 @@
 @section('head')
 <meta name="csrf-token" content="{{ csrf_token() }}">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.0/css/all.min.css" integrity="sha512-DxV+EoADOkOygM4IR9yXP8Sb2qwgidEmeqAEmDKIOfPRQZOWbXCzLC6vjbZyy0vPisbH2SyW27+ddLVCN+OMzQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+<script src="https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js"
+        integrity="sha512-BNaRQnYJYiPSqHHDb58B0yaPfCu+Wgds8Gp/gU33kqBtgNS4tSPHuGibyoeqMV/TJlSKda6FXzoEyYGjTe+vXA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 @vite('resources/css/orderPage.css')
 @vite('resources/js/profile.js')
 @endsection
@@ -64,33 +67,6 @@
          </div>
          <div id="myModal" class="modal">
             <div class="modal-wrapper">
-                <svg width="578" height="732" viewBox="0 0 578 732" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <g filter="url(#filter0_d_802_127)">
-                        <mask id="path-1-inside-1_802_127" fill="white">
-                            <path
-                                d="M558 0C569.046 0 578 8.95431 578 20V351.006C577.845 351.002 577.69 351 577.535 351C565.988 351 556.627 361.074 556.627 373.5C556.627 385.926 565.988 396 577.535 396C577.69 396 577.845 395.997 578 395.993V708C578 719.046 569.046 728 558 728H20C8.95431 728 8.85911e-08 719.046 0 708V395.86C10.4537 394.616 18.585 385.081 18.585 373.5C18.585 361.919 10.4538 352.383 0 351.139V20C0 8.95431 8.95431 0 20 0H558Z" />
-                        </mask>
-                        <path
-                            d="M558 0C569.046 0 578 8.95431 578 20V351.006C577.845 351.002 577.69 351 577.535 351C565.988 351 556.627 361.074 556.627 373.5C556.627 385.926 565.988 396 577.535 396C577.69 396 577.845 395.997 578 395.993V708C578 719.046 569.046 728 558 728H20C8.95431 728 8.85911e-08 719.046 0 708V395.86C10.4537 394.616 18.585 385.081 18.585 373.5C18.585 361.919 10.4538 352.383 0 351.139V20C0 8.95431 8.95431 0 20 0H558Z"
-                            fill="white" />
-                        <path
-                            d="M578 20H580H578ZM578 351.006L577.953 353.005L580 353.053V351.006H578ZM577.535 351V349V351ZM577.535 396V398V396ZM578 395.993H580V393.946L577.953 393.994L578 395.993ZM558 728V730V728ZM0 708H-2H0ZM0 395.86L-0.236322 393.874L-2 394.084V395.86H0ZM0 351.139H-2V352.915L-0.23631 353.125L0 351.139ZM558 0V2C567.941 2 576 10.0589 576 20H578H580C580 7.84974 570.15 -2 558 -2V0ZM578 20H576V351.006H578H580V20H578ZM578 351.006L578.047 349.006C577.88 349.003 577.709 349 577.535 349V351V353C577.672 353 577.81 353.002 577.953 353.005L578 351.006ZM577.535 351V349C564.748 349 554.627 360.11 554.627 373.5H556.627H558.627C558.627 362.037 567.228 353 577.535 353V351ZM556.627 373.5H554.627C554.627 386.89 564.748 398 577.535 398V396V394C567.228 394 558.627 384.963 558.627 373.5H556.627ZM577.535 396V398C577.716 398 577.891 397.996 578.047 397.993L578 395.993L577.953 393.994C577.8 393.997 577.665 394 577.535 394V396ZM578 395.993H576V708H578H580V395.993H578ZM578 708H576C576 717.941 567.941 726 558 726V728V730C570.15 730 580 720.15 580 708H578ZM558 728V726H20V728V730H558V728ZM20 728V726C10.0589 726 2 717.941 2 708H0H-2C-2 720.15 7.84974 730 20 730V728ZM0 708H2V395.86H0H-2V708H0ZM0 395.86L0.236322 397.846C11.7969 396.471 20.585 385.989 20.585 373.5H18.585H16.585C16.585 384.173 9.11062 392.762 -0.236322 393.874L0 395.86ZM18.585 373.5H20.585C20.585 361.011 11.797 350.528 0.23631 349.153L0 351.139L-0.23631 353.125C9.11056 354.237 16.585 362.827 16.585 373.5H18.585ZM0 351.139H2V20H0H-2V351.139H0ZM0 20H2C2 10.0589 10.0589 2 20 2V0V-2C7.84974 -2 -2 7.84974 -2 20H0ZM20 0V2H558V0V-2H20V0Z"
-                            fill="#1D1D1D" mask="url(#path-1-inside-1_802_127)" />
-                    </g>
-                    <defs>
-                        <filter id="filter0_d_802_127" x="0" y="0" width="578" height="732"
-                            filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-                            <feFlood flood-opacity="0" result="BackgroundImageFix" />
-                            <feColorMatrix in="SourceAlpha" type="matrix"
-                                values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
-                            <feOffset dy="4" />
-                            <feComposite in2="hardAlpha" operator="out" />
-                            <feColorMatrix type="matrix" values="0 0 0 0 0.1125 0 0 0 0 0.1125 0 0 0 0 0.1125 0 0 0 1 0" />
-                            <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_802_127" />
-                            <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_802_127" result="shape" />
-                        </filter>
-                    </defs>
-                </svg>
                 <div class="modal-content-overlay">
                     <div class="modal-top">
                         <span class="close"><img src="{{ asset('img/back-button.svg') }}" alt=""></span>
@@ -309,23 +285,33 @@
             var modal = document.getElementById("myModal");
             modal.style.display = "none";
         }
-        // screenshot (hide close/download controls during capture)
         function screenshot() {
             const captureElement = document.querySelector(".modal-content-overlay");
             if (!captureElement) return;
             const controls = Array.from(captureElement.querySelectorAll('.modal-top .close, .modal-top .downloadBtn'));
             const previousDisplay = controls.map(el => el.style.display);
             controls.forEach(el => { el.style.display = 'none'; });
-            html2canvas(captureElement).then(function(c) {
-                const url = c.toDataURL();
-                const linkEl = document.createElement("a");
-                linkEl.setAttribute("href", url);
-                linkEl.setAttribute("download", "receipt.png");
-                linkEl.click();
-                linkEl.remove();
-            }).finally(() => {
+            if (typeof html2canvas !== 'function') {
                 controls.forEach((el, i) => { el.style.display = previousDisplay[i] || ''; });
-            });
+                alert('Capture library not loaded. Please try again.');
+                return;
+            }
+            html2canvas(captureElement,{backgroundColor: '#ffffff'})
+                .then(function(c) {
+                    const url = c.toDataURL('image/png');
+                    const linkEl = document.createElement("a");
+                    linkEl.href = url;
+                    linkEl.download = 'receipt.png';
+                    document.body.appendChild(linkEl);
+                    linkEl.click();
+                    linkEl.remove();
+                })
+                .catch(() => {
+                    alert('Failed to capture. Please try again.');
+                })
+                .finally(() => {
+                    controls.forEach((el, i) => { el.style.display = previousDisplay[i] || ''; });
+                });
         }
 
         buttons.forEach(button => {
